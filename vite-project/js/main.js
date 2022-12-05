4a416a384127c3bfdba3cae23a041815640cedcc
annieeeeeeeeeeeee <annieou000@gmail.com>
2022-12-05 10:16:36 -0500

    3

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,3 +4,12 @@ import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
+
+
+document.getElementbyId("menu").insertAdjacentHtml;
+("beforeend",
+`<div class="card">
+    <h2>${menu.name}</h2>
+</div>);`
+
+/*console.log(menu); */
